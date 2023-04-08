4acb3c7bfdeb95944ba612a7e4704209f5ebdbf2
Kevin Bellinger <129331932+kbellinger7625@users.noreply.github.com>
2023-04-08 16:15:37 -0700

    Add files via upload

---
+++ Sample File for Project.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Downloading multiple programs...
+
+curl -L -o OperaGXSetup.exe https://net.geo.opera.com/opera_gx/stable/windows?utm_tryagain=yes&utm_source=google&utm_medium=ose&utm_campaign=(none)&http_referrer=https%3A%2F%2Fwww.google.com%2F&utm_site=opera_com&&utm_lastpage=opera.com/
+
+echo Download completed.
+pause
